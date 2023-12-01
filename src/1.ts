b18046be1c99ddc5c4e10c1cff7cb323ea0a12d9
Angela McEgo <eve.loikkanen@gmail.com>
2023-12-01 23:09:05 +0200

    More beautiful whitespace

--- src/1.ts
+++ src/1.ts
@@ -33,12 +33,10 @@ function CollectSum(digits:Record<string, number>) {
 const part1dict: Record<string, number> = {
    '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
 };
-
 CollectSum(part1dict);
 
 const part2dict: Record<string, number> = {
   '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
   'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9,
 };
-
 CollectSum(part2dict);
