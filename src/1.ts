86a9ddd21f9bfa5b5274d1f23e9dfcb2665a13d4
Angela McEgo <eve.loikkanen@gmail.com>
2023-12-02 19:19:58 +0200

    Made printing better in day 1

--- src/1.ts
+++ src/1.ts
@@ -27,16 +27,16 @@ function CollectSum(digits:Record<string, number>) {
     if (first != -1)
       sum += first*10+last;
   });
-  console.log(sum);
+  return sum;
 }
 
 const part1dict: Record<string, number> = {
    '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
 };
-CollectSum(part1dict);
+console.log("Part 1: "+ CollectSum(part1dict));
 
 const part2dict: Record<string, number> = {
   '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
   'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9,
 };
-CollectSum(part2dict);
+console.log("Part 2: "+ CollectSum(part2dict));
